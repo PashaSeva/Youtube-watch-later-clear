2ba90aa1a4b969cbe44c12d1328a357a3223f184
PashaSeva <117672642+PashaSeva@users.noreply.github.com>
2024-09-14 11:08:02 +0300

    Add files via upload

---
+++ Очистка посмотеть позже на ютуб .py
@@ -0,0 +1,15 @@
+import mouse, time
+
+time.sleep(5)
+while True:
+
+    mouse.move(1482, 370) #Первая кордината
+    time.sleep(1) 
+    mouse.click()
+    
+    time.sleep(1)
+    mouse.move(1350, 490) #Вторая кордината
+    time.sleep(1)
+    mouse.click()
+    time.sleep(1)
+    
